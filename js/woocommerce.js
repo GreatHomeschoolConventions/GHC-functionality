e5ec4ff2f3e7918f8bd0a34045923fb2d016ecd8
Andrew Minion <andrew@andrewrminion.com>
2016-09-23 15:51:43 -0400

    handle pluralization

--- js/woocommerce.js
+++ js/woocommerce.js
@@ -1,4 +1,15 @@
 (function($){
     $(document).ready(function(){
+
+        // fix pluralization on product pages and in cart
+        changePeopleAgreement();
+        $('.quantity input[type=number]').on('change', changePeopleAgreement);
+        function changePeopleAgreement() {
+            if ($('.quantity input[type=number]').val() == 1) {
+                $('.quantity .people').html('person');
+            } else {
+                $('.quantity .people').html('people');
+            }
+        }
     });
 })(jQuery);

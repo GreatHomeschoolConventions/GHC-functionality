7652906fb6582075b025e39b97e0fde4e89ee2a3
Andrew Minion <andrew@andrewrminion.com>
2016-10-27 09:58:43 -0400

    tweak size and fix typo

--- ghc-speakers.php
+++ ghc-speakers.php
@@ -40,7 +40,7 @@ register_deactivation_hook( __FILE__, 'ghc_speakers_deactivate' );
 // add custom image sizes
 add_action( 'after_setup_theme', 'ghc_custom_image_sizes' );
 function ghc_custom_image_sizes() {
-    add_image_size( 'thumbnail-no-crop', 150, 150, false );
+    add_image_size( 'thumbnail-no-crop', 140, 140, false );
     add_image_size( 'square-small', 400, 400, true );
     add_image_size( 'square-medium', 600, 600, true );
     add_image_size( 'square-large', 900, 900, true );
@@ -52,7 +52,7 @@ function ghc_custom_image_sizes() {
 add_filter( 'image_size_names_choose', 'ghc_custom_image_sizes_names' );
 function ghc_custom_image_sizes_names( $sizes ) {
     return array_merge( $sizes, array(
-        'thumbnail-no-crop'         => 'Thumbainl (no crop)',
+        'thumbnail-no-crop'         => 'Thumbnail (no crop)',
         'square-small'              => 'Square',
         'square-medium'             => 'Square',
         'square-large'              => 'Square',

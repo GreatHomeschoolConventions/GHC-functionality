147b6cd96ec20eacf2e5c9e6f2380956a67df537
Andrew Minion <andrew@andrewrminion.com>
2016-10-28 11:32:41 -0400

    add map data for future use

--- inc/hotel-template.php
+++ inc/hotel-template.php
@@ -37,8 +37,9 @@
                     if ( get_field( 'location' ) ) {
                         $location = get_field( 'location' );
 
-                        $convention_address = $conventions[$this_convention]['address'][0] . '+' . $conventions[$this_convention]['city'][0] . ',+' . $conventions[$this_convention]['state'][0] . '+' . $conventions[$this_convention]['zip'][0];
-                        echo '<p><a target="_blank" href="https://www.google.com/maps/dir/' . str_replace( ' ', '+', $location['address'] ) . '/' . $convention_address . '/">Directions to ' . $conventions[$this_convention]['convention_center_name'][0] . ' <span class="fa fa-map"></span></a></p>';
+                        $convention_address = $conventions[$this_convention]['address'][0] . ' ' . $conventions[$this_convention]['city'][0] . ', ' . $conventions[$this_convention]['state'][0] . ' ' . $conventions[$this_convention]['zip'][0];
+                        echo '<p><a target="_blank" href="https://www.google.com/maps/dir/' . str_replace( ' ', '+', $location['address'] ) . '/' . str_replace( ' ', '+', $convention_address ) . '/">Directions to ' . $conventions[$this_convention]['convention_center_name'][0] . ' <span class="fa fa-map"></span></a></p>
+                        <div class="map" data-source-address="' . $location['address'] . '" data-destination-address="' . $convention_address . '"></div>';
                     }
                 ?></div>
             </div>

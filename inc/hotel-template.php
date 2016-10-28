f0e25805de2dff5db315857d5e9bf7b0f819993b
Andrew Minion <andrew@andrewrminion.com>
2016-10-28 10:29:06 -0400

    add convention class to each hotel for easier filtering in the future

--- inc/hotel-template.php
+++ inc/hotel-template.php
@@ -1,4 +1,10 @@
-<div class="four columns">
+<?php
+    if ( is_post_type_archive( 'hotel' ) ) {
+        $conventions_taxonomy = get_the_terms( get_the_ID(), 'ghc_conventions_taxonomy' );
+        $this_convention = array_flip( $convention_abbreviations )[$conventions_taxonomy[0]->slug];
+    }
+?>
+<div class="four columns <?php echo $this_convention; ?>">
     <div class="gdlr-item gdlr-accommodation-item hotel <?php echo get_field( 'sold_out' ) ? 'sold-out' : ''; ?>">
         <?php if ( has_post_thumbnail() ) { ?>
         <div class="accommodation-thumbnail"><?php the_post_thumbnail( 'small-grid-size' ); ?></div>
@@ -7,8 +13,7 @@
             <div class="accommodation-content-wrapper">
                 <?php
                 if ( is_post_type_archive( 'hotel' ) ) {
-                    $conventions = get_the_terms( get_the_ID(), 'ghc_conventions_taxonomy' );
-                    echo do_shortcode( '[convention_icon convention="' . $conventions[0]->slug . '"]' );
+                    echo do_shortcode( '[convention_icon convention="' . $this_convention . '"]' );
                 }
                 ?>
                 <h3 class="accommodation-title"><?php the_title(); ?></h3>

c6556d6e87ecf03a50fd7608be426398682803ec
Andrew Minion <andrew@andrewrminion.com>
2016-11-10 15:54:46 -0500

    add hotel scam alert

--- inc/loop-hotel.php
+++ inc/loop-hotel.php
@@ -29,7 +29,12 @@ if ( $this_convention ) {
 $hotel_query = new WP_Query( $hotel_args );
 
 // the loop
-if ( $hotel_query->have_posts() ) {
+if ( $hotel_query->have_posts() ) { ?>
+    <h2>Hotel Scam Alert</h2>
+    <p>We have received word that a company by the name of Expo Housing Services is making calls in regard to Great Homeschool Conventions&rsquo; hotel accommodations. They do this each year; it is a scam from a company trying to book hotel rooms on behalf of Great Homeschool Conventions.</p>
+    <p>We have nothing to do with this company and they do not represent Great Homeschool Conventions in any way.</p>
+    <p>Please book your accommodations through one of the options below.</p>
+    <?php
     $counter = 1;
     while ( $hotel_query->have_posts() ) {
         $hotel_query->the_post();

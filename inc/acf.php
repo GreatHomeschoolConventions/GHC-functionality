ff3a7377f6aba650950e031fc1032d69044f0601
Andrew Minion <andrew@andrewrminion.com>
2017-08-29 22:43:49 -0400

    add conditional function wrappers

--- inc/acf.php
+++ inc/acf.php
@@ -5,6 +5,7 @@ defined( 'ABSPATH' ) or die( 'No access allowed' );
 /**
  * Add ACF options page
  */
+if ( ! function_exists( 'ghc_admin_options' ) ) {
 function ghc_admin_options() {
     if ( function_exists( 'acf_add_options_page' ) ) {
         acf_add_options_page(array(
@@ -16,13 +17,16 @@ function ghc_admin_options() {
         ));
     }
 }
+}
 add_action( 'after_setup_theme', 'ghc_admin_options' );
 
 /**
  * Add Google Maps API key for ACF use
  */
+if ( ! function_exists( 'ghc_acf_init' ) ) {
 function ghc_acf_init() {
     acf_update_setting( 'google_api_key', get_option( 'options_api_key' ) );
+}
 }
 add_action( 'acf/init', 'ghc_acf_init' );
 
@@ -31,18 +35,22 @@ add_action( 'acf/init', 'ghc_acf_init' );
  * @param  string $path ACF local JSON save directory
  * @return string ACF local JSON save directory
  */
-add_filter( 'acf/settings/save_json', 'ghc_acf_json_save_point' );
+if ( ! function_exists( 'ghc_acf_json_save_point' ) ) {
 function ghc_acf_json_save_point( $path ) {
     return plugin_dir_path( __FILE__ ) . '/../acf-json';
 }
+}
+add_filter( 'acf/settings/save_json', 'ghc_acf_json_save_point' );
 
 /**
  * Set ACF local JSON open directory
  * @param  array $path ACF local JSON open directory
  * @return array ACF local JSON open directory
  */
-add_filter( 'acf/settings/load_json', 'ghc_acf_json_load_point' );
+if ( ! function_exists( 'ghc_acf_json_load_point' ) ) {
 function ghc_acf_json_load_point( $path ) {
     $paths[] = plugin_dir_path( __FILE__ ) . '/../acf-json';
     return $paths;
 }
+}
+add_filter( 'acf/settings/load_json', 'ghc_acf_json_load_point' );

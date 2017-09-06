14276ae7a9fd21bc2e2c6e1ec15911e115e3cb8e
Andrew Minion <andrew@andrewrminion.com>
2017-09-06 17:15:36 -0400

    add shortcode "show" attributes to article class list

--- templates/speaker-template.php
+++ templates/speaker-template.php
@@ -7,7 +7,7 @@ if ( ! is_array( $shortcode_attributes ) ) {
         $thumbnail_size = 'large';
     }
 }
-?><article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+?><article id="post-<?php the_ID(); ?>" <?php post_class( 'contains-' . str_replace( ',', ' contains-', esc_attr( $shortcode_attributes['show'] ) ) ); ?>>
     <?php if ( has_post_thumbnail() && strpos( $shortcode_attributes['show'], 'image' ) !== false ) { ?>
     <div class="speaker-thumbnail">
         <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php

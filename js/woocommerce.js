3633e48e50e31a772bb8eccd6bd8063264eb8999
Andrew Minion <andrew@andrewrminion.com>
2017-09-19 16:00:53 -0400

    move functions to top of file

--- js/woocommerce.js
+++ js/woocommerce.js
@@ -1,20 +1,64 @@
 (function($){
+    /**
+     * Fix quantity pluralization
+     */
+    function changePeopleAgreement() {
+        if ($('.quantity input[type=number]').val() == 1) {
+            $('.quantity .people').html('person');
+        } else {
+            $('.quantity .people').html('people');
+        }
+    }
+
+    /**
+     * Show/hide family vs. individual products
+     */
+    function checkFamilySelection() {
+        var selectedTicketType = $('.woocommerce-content > .product_cat-registration select[name="attribute_attendee-type"]').val();
+        if (selectedTicketType.indexOf('Family') > -1) {
+            $('.product-addon-family-members').slideDown();
+            $('option[value="1-individual-1"]').hide();
+            $('.product-addon-family-members select.addon').val('');
+        } else {
+            $('.product-addon-family-members').slideUp();
+            $('option[value="1-individual-1"]').show();
+            $('.product-addon-family-members select.addon').val('1-individual-1');
+        }
+    }
+
+    /**
+     * If value is out of allowed min-max range, bring it back into the valid range
+     */
+    function fixMaxTickets(inputField) {
+        if (Number(inputField.attr('max')) > 0 && Number(inputField.val()) > Number(inputField.attr('max'))) {
+            inputField.val(inputField.attr('max'));
+        }
+        if (Number(inputField.val()) < Number(inputField.attr('min'))) {
+            inputField.val(inputField.attr('min'));
+        }
+    }
+
+    /**
+     * Show/hide gift recipient info
+     */
+    function handleGiftMemberships() {
+        // handle gift vs. normal purchases
+        if ($('.product-addon-gift-options input[type="checkbox"]').is(':checked') == true) {
+            // show recipient info and make it required
+            $('.product-addon-recipient-information').slideDown();
+            $('.product-addon-recipient-information input').attr('required', true);
+        } else {
+            // hide and don’t require recipient info
+            $('.product-addon-recipient-information').slideUp();
+            $('.product-addon-recipient-information input').attr('required', false);
+        }
+    }
+
     $(document).ready(function(){
 
         // fix pluralization on product pages and in cart
         changePeopleAgreement();
         $('.quantity input[type=number]').on('change', changePeopleAgreement);
-
-        /**
-         * Fix quantity pluralization
-         */
-        function changePeopleAgreement() {
-            if ($('.quantity input[type=number]').val() == 1) {
-                $('.quantity .people').html('person');
-            } else {
-                $('.quantity .people').html('people');
-            }
-        }
 
         // show/hide family member quantity depending on registration type
         if ($('.woocommerce-content > .product_cat-registration').length > 0) {
@@ -22,42 +66,10 @@
             $('body').on('change', '.product_cat-registration select[name="attribute_attendee-type"]', checkFamilySelection);
         }
 
-        /**
-         * Show/hide family vs. individual products
-         */
-        function checkFamilySelection() {
-            var selectedTicketType = $('.woocommerce-content > .product_cat-registration select[name="attribute_attendee-type"]').val();
-            if (selectedTicketType.indexOf('Family') > -1) {
-                $('.product-addon-family-members').slideDown();
-                $('option[value="1-individual-1"]').hide();
-                $('.product-addon-family-members select.addon').val('');
-            } else {
-                $('.product-addon-family-members').slideUp();
-                $('option[value="1-individual-1"]').show();
-                $('.product-addon-family-members select.addon').val('1-individual-1');
-            }
-        }
-
         // show/hide gift recipient info
         if ($('.woocommerce-content > .product_cat-registration').length > 0) {
             handleGiftMemberships();
             $('body').on('change', '.product-addon-gift-options input[type="checkbox"]', handleGiftMemberships);
-        }
-
-        /**
-         * Show/hide gift recipient info
-         */
-        function handleGiftMemberships() {
-            // handle gift vs. normal purchases
-            if ($('.product-addon-gift-options input[type="checkbox"]').is(':checked') == true) {
-                // show recipient info and make it required
-                $('.product-addon-recipient-information').slideDown();
-                $('.product-addon-recipient-information input').attr('required', true);
-            } else {
-                // hide and don’t require recipient info
-                $('.product-addon-recipient-information').slideUp();
-                $('.product-addon-recipient-information input').attr('required', false);
-            }
         }
 
         // hide “family members” line from individual registrations in the cart and handle program guides
@@ -73,15 +85,11 @@
             $('.cart_item:contains("Additional Program Guide")').find('.attendees-count').remove();
         }
 
-        // fix for browsers allowing out-of-range inputs (Safari)
-        if (($('input.qty[max], input.qty[min]').length > 0) || ($('input.qty[min] ').length > 0)) {
-            $('input.qty[max]').on('change', function() {
-                if (Number($(this).attr('max')) > 0 && Number($(this).val()) > Number($(this).attr('max'))) {
-                    $(this).val($(this).attr('max'));
-                }
-                if (Number($(this).val()) < Number($(this).attr('min'))) {
-                    $(this).val($(this).attr('min'));
-                }
+        // fix for browsers allowing out-of-range inputs
+        if (($('input.qty[max], input.qty[min]').length > 0) || ($('input.qty[min]').length > 0)) {
+            $('input.qty[max], input.qty[min]').on('change', fixMaxTickets($(this)));
+            $('input.qty[max], input.qty[min]').each(function() {
+                fixMaxTickets($(this))
             });
         }
 

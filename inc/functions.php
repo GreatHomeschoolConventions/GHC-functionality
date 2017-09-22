53f74d52bc37819ad00c310368e83c06d82f9eb6
Andrew Minion <andrew@andrewrminion.com>
2017-09-22 15:36:17 -0400

    add checkout fields placeholders

--- inc/functions.php
+++ inc/functions.php
@@ -424,3 +424,23 @@ function woocommerce_header_add_to_cart_fragment( $fragments ) {
 
 }
 add_filter('woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment');
+
+/**
+ * Add placeholders to all checkout fields
+ * @param  array $fields all checkout fields
+ * @return array modified checkout fields
+ */
+function woocommerce_checkout_fields_placeholders( $fields ) {
+    $fields['billing']['billing_first_name']['placeholder'] = 'John';
+    $fields['billing']['billing_last_name']['placeholder'] = 'Doe';
+    $fields['billing']['billing_company']['placeholder'] = 'ACME Inc.';
+    $fields['billing']['billing_address_1']['placeholder'] = '123 Anystreet';
+    $fields['billing']['billing_address_2']['placeholder'] = 'Suite 1001';
+    $fields['billing']['billing_city']['placeholder'] = 'Anytown';
+    $fields['billing']['billing_postcode']['placeholder'] = '12345';
+    $fields['billing']['billing_phone']['placeholder'] = '234-567-8901';
+    $fields['billing']['billing_email']['placeholder'] = 'john.doe@example.com';
+
+    return $fields;
+}
+add_filter( 'woocommerce_checkout_fields', 'woocommerce_checkout_fields_placeholders' );

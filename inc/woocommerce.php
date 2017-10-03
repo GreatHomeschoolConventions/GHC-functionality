f1672bef2d979d28c442ded0d2c9a333f053c32f
Andrew Minion <andrew@andrewrminion.com>
2017-10-03 15:01:17 -0400

    clean up extra files

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -3,7 +3,7 @@
 defined( 'ABSPATH' ) or die( 'No access allowed' );
 
 /**
- * Show product short description on registration page
+ * Show product short description on product archive
  */
 add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_single_excerpt', 5);
 
@@ -263,3 +263,65 @@ function ghc_set_convention_variation_IDs_transient() {
 
 add_action( 'save_post_product', 'ghc_set_convention_variation_IDs_transient' );
 add_action( 'save_post_product_variation', 'ghc_set_convention_variation_IDs_transient' );
+
+/**
+ * Add custom cart total location
+ * @param  array $fragments array of cart total areas
+ * @return array modified array of cart total areas
+ */
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+    global $woocommerce;
+
+    ob_start();
+
+    ?>
+    <span class="custom-cart-total"><?php echo $woocommerce->cart->get_cart_total(); ?></span>
+    <?php
+
+    $fragments['.custom-cart-total'] = ob_get_clean();
+
+    return $fragments;
+
+}
+add_filter('woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment');
+
+/**
+ * Add placeholders to all checkout fields
+ * @param  array $fields all checkout fields
+ * @return array modified checkout fields
+ */
+function woocommerce_checkout_fields_placeholders( $fields ) {
+    $fields['billing']['billing_first_name']['placeholder'] = 'John';
+    $fields['billing']['billing_last_name']['placeholder'] = 'Doe';
+    $fields['billing']['billing_company']['placeholder'] = 'ACME Inc.';
+    $fields['billing']['billing_address_1']['placeholder'] = '123 Anystreet';
+    $fields['billing']['billing_address_2']['placeholder'] = 'Suite 1001';
+    $fields['billing']['billing_city']['placeholder'] = 'Anytown';
+    $fields['billing']['billing_postcode']['placeholder'] = '12345';
+    $fields['billing']['billing_phone']['label'] = 'Cell Phone';
+    $fields['billing']['billing_phone']['placeholder'] = '234-567-8901';
+    $fields['billing']['billing_email']['placeholder'] = 'john.doe@example.com';
+
+    return $fields;
+}
+add_filter( 'woocommerce_checkout_fields', 'woocommerce_checkout_fields_placeholders' );
+
+/**
+ * Tweak return customer login message
+ * @param string $message returning customer string
+ * @param string modified message
+ */
+add_filter( 'woocommerce_checkout_login_message', function() {return 'Been to a GHC convention before?';} );
+
+/**
+ * Auto-complete all orders
+ */
+function ghc_auto_complete_order( $order_id ) {
+    if ( ! $order_id ) {
+        return;
+    }
+
+    $order = wc_get_order( $order_id );
+    $order->update_status( 'completed' );
+}
+add_action( 'woocommerce_thankyou', 'ghc_auto_complete_order' );

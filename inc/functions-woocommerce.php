11bf88aa99af700f104ec1928ce1b5d40f4ce7f0
Andrew Minion <andrew@andrewrminion.com>
2017-10-03 09:42:54 -0400

    auto-complete all orders

--- inc/functions-woocommerce.php
+++ inc/functions-woocommerce.php
@@ -50,3 +50,16 @@ add_filter( 'woocommerce_checkout_fields', 'woocommerce_checkout_fields_placehol
  * @param string modified message
  */
 add_filter( 'woocommerce_checkout_login_message', function() {return 'Been to a GHC convention before?';} );
+
+/**
+ * Auto-complete all orders
+ */
+function ghc_auto_complete_order( $order_id ) {
+    if ( ! $order_id ) {
+        return;
+    }
+
+    $order = wc_get_order( $order_id );
+    $order->update_status( 'completed' );
+}
+add_action( 'woocommerce_thankyou', 'ghc_auto_complete_order' );

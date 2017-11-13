48bba25f9874324ba6e35d10baed491129f3d5bb
Andrew Minion <andrew@andrewrminion.com>
2017-11-13 12:53:50 -0500

    remove special events from speakers page

--- templates/loop-speaker.php
+++ templates/loop-speaker.php
@@ -1,37 +1,11 @@
 <?php
-    // how many speakers to show above entertainers
-    $top_speakers_quantity = 20;
-
     // new queries
     $top_speakers_args = array (
         'post_type'              => 'speaker',
         'pagination'             => false,
-        'posts_per_archive_page' => $top_speakers_quantity,
-        'order'                  => 'ASC',
-        'orderby'                => 'menu_order',
-    );
-    $entertainers_args = array (
-        'post_type'              => 'special_event',
-        'pagination'             => false,
-        'order'                  => 'ASC',
-        'orderby'                => 'menu_order',
-    );
-
-    // the queries
-    $top_speakers_query = new WP_Query( $top_speakers_args );
-    $entertainers_query = new WP_Query( $entertainers_args );
-
-    // get IDs of top speakers so they aren’t included again in the bottom randomly-ordered section
-    $top_speakers_IDs = wp_list_pluck( $top_speakers_query->posts, 'ID' );
-
-    $bottom_speakers_args = array (
-        'post_type'              => 'speaker',
-        'pagination'             => false,
         'posts_per_archive_page' => -1,
-        'offset'                 => $top_speakers_quantity,
         'order'                  => 'ASC',
-        'orderby'                => 'rand',
-        'post__not_in'           => $top_speakers_IDs,
+        'orderby'                => 'menu_order',
         'tax_query'         => array(
             array(
                 'taxonomy'  => 'ghc_speaker_category_taxonomy',
@@ -41,7 +15,8 @@
         ),
     );
 
-    $bottom_speakers_query = new WP_Query( $bottom_speakers_args );
+    // the queries
+    $top_speakers_query = new WP_Query( $top_speakers_args );
 
     // top speakers (set with $top_speakers_quantity)
     if ( $top_speakers_query->have_posts() ) {
@@ -55,28 +30,6 @@
     } else {
         echo '<h2>More information coming soon.</h2>';
         echo do_shortcode( '[contact-form-7 id="28288" title="Signup - Convention Info"]' );
-    }
-
-    // entertainers
-    if ( $entertainers_query->have_posts() ) {
-        echo '<div class="special_event-container ghc-cpt container">';
-        while ( $entertainers_query->have_posts() ) {
-            $entertainers_query->the_post();
-            $thumbnail_size = 'special-event-large';
-            include('speaker-template.php');
-        }
-        echo '</div><!-- .special_event-container.ghc-cpt.container -->';
-    }
-
-    // bottom speakers
-    if ( $bottom_speakers_query->have_posts() ) {
-        echo '<div class="speaker-container ghc-cpt container">';
-        while ( $bottom_speakers_query->have_posts() ) {
-            $bottom_speakers_query->the_post();
-            $thumbnail_size = 'medium';
-            include('speaker-template.php');
-        }
-        echo '</div><!-- .speaker-container.ghc-cpt.container -->';
     }
 
     // Restore original post data

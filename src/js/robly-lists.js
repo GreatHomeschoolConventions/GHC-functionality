f562bb220cd2b851fbba7d240fd359358ede2110
Andrew Minion <andrew@andrewrminion.com>
2018-05-07 16:51:59 -0400

    add NY and FL conventions

--- src/js/robly-lists.js
+++ src/js/robly-lists.js
@@ -1,33 +1,49 @@
-(function($){
-    $(document).ready(function() {
-        $('input[type="checkbox"][name*="locations-of-interest"]').on('change', function() {
-            var $roblyListsInput = $('input[name="robly-lists"]'),
-                thisVal = $(this).val(),
-                currentVal = $roblyListsInput.val().split(','),
-                thisValIndex;
+/**
+ * Update hidden Robly lists field with select values
+ *
+ * @package GHC_Functionality_Plugin
+ */
 
-                if (thisVal.indexOf('Greenville') > -1) {
-                    var thisConferenceListId = '11159';
-                } else if (thisVal.indexOf('Fort Worth') > -1) {
-                    var thisConferenceListId = '11161';
-                } else if (thisVal.indexOf('Cincinnati') > -1) {
-                    var thisConferenceListId = '11157';
-                } else if (thisVal.indexOf('Ontario') > -1) {
-                    var thisConferenceListId = '11163';
-                } else if (thisVal.indexOf('St. Charles') > -1) {
-                    var thisConferenceListId = '222196';
-                }
+/*global
+	document, jQuery
+*/
 
-            if ($(this).attr('checked')) {
-                currentVal.push(thisConferenceListId);
-            } else {
-                thisValIndex = currentVal.indexOf(thisConferenceListId);
-                if (thisValIndex > -1) {
-                    currentVal.splice(thisValIndex, 1);
-                }
-            }
+(function ($) {
+	"use strict";
+	$(document).ready(function () {
+		$('input[type="checkbox"][name*="locations-of-interest"]').on('change', function () {
+			var $roblyListsInput = $('input[name="robly-lists"]'),
+				thisVal = $(this).val(),
+				currentVal = $roblyListsInput.val().split(','),
+				thisValIndex,
+				thisConferenceListId = '';
 
-            $roblyListsInput.val(currentVal.join());
-        });
-    });
-})(jQuery);
+			if (thisVal.indexOf('Greenville') > -1) {
+				thisConferenceListId = '11159';
+			} else if (thisVal.indexOf('Fort Worth') > -1) {
+				thisConferenceListId = '11161';
+			} else if (thisVal.indexOf('Cincinnati') > -1) {
+				thisConferenceListId = '11157';
+			} else if (thisVal.indexOf('Ontario') > -1) {
+				thisConferenceListId = '11163';
+			} else if (thisVal.indexOf('St. Charles') > -1) {
+				thisConferenceListId = '222196';
+			} else if (thisVal.indexOf('New York') > -1) {
+				thisConferenceListId = '305507';
+			} else if (thisVal.indexOf('Florida') > -1) {
+				thisConferenceListId = '11174';
+			}
+
+			if ($(this).attr('checked')) {
+				currentVal.push(thisConferenceListId);
+			} else {
+				thisValIndex = currentVal.indexOf(thisConferenceListId);
+				if (thisValIndex > -1) {
+					currentVal.splice(thisValIndex, 1);
+				}
+			}
+
+			$roblyListsInput.val(currentVal.join());
+		});
+	});
+}(jQuery));
